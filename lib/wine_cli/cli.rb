9d27e90192483e546b8696af0f058ec0ee0b7226
Kazumi Karbowski <kazkazinus@gmail.com>
2018-07-21 13:28:16 +0000

    Define goodbye

--- lib/wine_cli/cli.rb
+++ lib/wine_cli/cli.rb
@@ -3,7 +3,9 @@ class WineCli::CLI
   def start
     puts "Welcome to Wine Pairing App!"
     puts ""
-    WineCli::Scraper.scrape_details #<- this is here only for test purposes
+    list
+    menu
+    goodbye
   end
 
   def list
@@ -14,17 +16,50 @@ class WineCli::CLI
   end
 
   def menu
-    puts "Please select a wine varietal to view more information:"
+    puts "Please select a wine varietal to view more information or type exit:"
     puts ""
-
-
+    input = nil
+    while input != "exit"
     input = gets.strip
-    input = input.to_i - 1
-# binding.pry
-
+    # input = input.to_i - 1
+    case input
+    when "1"
+      puts "More info on 1"  #<- I think I want it to say "More info on 1. Pinot Noir"
+    when "2"
+      puts "More info on 2"
+    when "3"
+      puts "More info on 3"
+    when "4"
+      puts "More info on 4"
+    when "5"
+      puts "More info on 5"
+    when "6"
+      puts "More info on 6"
+    when "7"
+      puts "More info on 7"
+    when "8"
+      puts "More info on 8"
+    when "9"
+      puts "More info on 9"
+    when "10"
+      puts "More info on 10"
+    when "11"
+      puts "More info on 11"
+    when "12"
+      puts "More info on 12"
+    when "13"
+      puts "More info on 13"
+    when "14"
+      puts "More info on 14"
+    when "15"
+      puts "More info on 15"
+      end
+    end
   end
 
-
+  def goodbye
+    puts "Cheers and goodbye!"
+  end
 
 
 end
